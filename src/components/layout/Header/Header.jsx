297e9f0a07fd4f83295b5ce80aa7991eb2dde5a1
auxaneN <a.nitharum@sfr.fr>
2020-03-12 12:29:47 -0400

    Fixed credit links

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.scss';
 export const Header = () => (
   <header className="Header">
     <h2>Hasty's texties</h2>
-    <a href="https://twitter.com/casyus">Art and characters @ Hastylion</a><br/>
+    <a href="https://twitter.com/hastylion">Art and characters @ Hastylion</a><br/>
     <a href="https://twitter.com/hastylion">Web app @ Cell</a>
   </header>
 )
